f74d6cec66798cf2d7aef2c1f943e77d1f8e3616
Viết Duy Lê <Duy.LeViet1@Nashtechglobal.com>
2020-03-10 18:52:30 +0700

    Implement middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { NotesModule } from './components/notes/notes.module';
 import { TodosModule } from './components/todos/todos.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [NotesModule, TodosModule]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      //.forRoutes('todos');    //restricted to said route only
+  }
+}
